524d9efded1ba2d9c749920cf71cfc1de1e3b80d
Yi Zhao <yiizhao@berkeley.edu>
2021-07-17 02:21:00 +0000

    final

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -64,11 +64,15 @@ class MoviesController < ApplicationController
 #       return
 #     end
     if session[:ratings] != params[:ratings]
-       session[:ratings] = params[:ratings]
-       redirect_to movies_path(params)
+      session[:ratings] = params[:ratings]
+      redirect_to movies_path(params)
     elsif session[:sort_by] != params[:sort_by]
-       session[:sort_by] = params[:sort_by]
-       redirect_to movies_path(params)      
+      if params[:sort_by].nil?
+        redirect_to movies_path(:ratings => params[:ratings], :sort_by => session[sort_by])       
+      else
+         session[:sort_by] = params[:sort_by]
+         redirect_to movies_path(params)
+      end
     else
        @ratings_to_show = params[:ratings].keys
        @sort_by = params[:sort_by]
